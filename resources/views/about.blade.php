c8c03156e359a2d0ee33f01074f976a56a3eec35
Svetlana Lozan <svetiklana94@gmail.com>
2024-01-20 21:14:58 +0200

    feat: add layout and shared dir

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,10 @@
-<h1>О боги</h1>
-<p>эксперименты с ларавелью на хекслете</p>
-@foreach ($tags as $pup)
-    <p>kukaracha pupla {{$pup}}</p>
-@endforeach
+@extends('layouts.app')
+
+@section('title','ВЗРЫВ МОЗГА.Ч.1')
+@section('header', 'О боги')
+@section('content')
+    <p>эксперименты с ларавелью на хекслете</p>
+    @foreach ($tags as $pup)
+        <p>kukaracha pupla {{$pup}}</p>
+    @endforeach
+@endsection
